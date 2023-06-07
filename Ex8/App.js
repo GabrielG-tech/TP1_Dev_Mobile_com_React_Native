224f273dcadba964e06b4e2eaefa18f32d329254
Gabriel <gabriel@mpsouza.com.br>
2023-06-07 15:05:40 -0300

    Ex8

--- Ex8/App.js
+++ Ex8/App.js
@@ -1,11 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { useState, useEffect } from 'react';
 
 export default function App() {
+  const data = ['John', 'Paul', 'Ringo', 'George'];
   return (
     <View style={styles.container}>
-      <Text>A fazer...</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.titulo}>Membros dos Beatles</Text>
+      <FlatList
+        style={styles.lista}
+        data={data}
+        renderItem={({item}) => <Text>{item} </Text>}
+        keyExtractor={item => item.id}
+      />
     </View>
   );
 }
@@ -14,7 +20,14 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 26,
   },
+  titulo: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    margin: 8,
+  },
+  lista: {
+    fontSize: 14,
+  }
 });
